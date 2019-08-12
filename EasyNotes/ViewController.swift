664a9ca0b6c62ce93b5d3de24a4329dd97196ac5
rupikanandamuri <rupikachowdhary09@gmail.com>
2019-08-12 09:43:19 -0400

    Initial Commit

---
+++ EasyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyNotes
+//
+//  Created by Rupika on 2019-08-12.
+//  Copyright © 2019 Rupika. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
